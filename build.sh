c3494e0611692f6c85fe074c9ee68bb353017b08
gnuhub <gnuhub@gmail.com>
2015-01-24 08:50:05 +0800

    T108#1433

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
-
+# 开启调试
+set -x
+# 获取配置信息
+. ./metadata
+# 开始构建
 pkgbuild --root root \
 	--scripts scripts \
-	--identifier com.gnuhub.user.stallman \
-	--version 0.1 \
-	create-user-stallman.pkg
+	--identifier "${identifier}" \
+	--version "${version}" \
+	"${name}_${version}.pkg"
